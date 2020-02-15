3a348917e243ca3ff80f51e172b1074410b38e6a
Nick Reyno <nickrenoe@gmail.com>
2020-02-15 15:30:12 -0500

    form events lookin mighty fine

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
+app = {};
 
+app.init = function() {
+	app.getDrinksByName();
+    app.getDrinksByAlcohol();
+    app.getDrinksByIngredient();
+};
+
+$(function() {
+	app.init();
+});
+
+app.getDrinksByName = function() {
+	$("form.drinksByNameForm").on("submit", function(e) {
+        e.preventDefault();
+        const userDrinksInput = $("#drinksByName").val();
+		$.ajax({
+			url: "https://www.thecocktaildb.com/api/json/v1/1/search.php",
+			method: "GET",
+			dataType: "json",
+			data: {
+				s: userDrinksInput
+			}
+		}).then(function(response) {
+			console.log(response);
+		});
+	});
+};
+
+app.getDrinksByAlcohol = function() {
+	$("form.drinksByAlcoholForm").on("submit", function(e) {
+        e.preventDefault();
+		const userAlcoholInput = $("#drinksByAlcohol").val();
+		$.ajax({
+			url: "https://www.thecocktaildb.com/api/json/v1/1/filter.php",
+			method: "GET",
+			dataType: "json",
+			data: {
+				i: userAlcoholInput
+			}
+		}).then(function(response) {
+			console.log(response);
+		});
+	});
+};
+
+app.getDrinksByIngredient = function() {
+	$("form.drinksByIngredientForm").on("submit", function(e) {
+        e.preventDefault();
+		const userIngredientInput1 = $(
+			"#drinksByIngredient1"
+		).val();
+		const userIngredientInput2 = $(
+			"#drinksByIngredient2"
+		).val();
+		$.ajax({
+			url: "https://www.thecocktaildb.com/api/json/v1/1/filter.php",
+			method: "GET",
+			dataType: "json",
+			data: {
+				i: userIngredientInput1
+			}
+		}).then(function(response) {
+			console.log(response);
+		});
+	});
+};
+
+app.populateGallery = function(drinkTitle, drinkUrl) {};
+
+app.populateSpotlight = function(spotlightID) {
+	$.ajax({
+		url: "https://www.thecocktaildb.com/api/json/v1/1/lookup.php",
+		method: "GET",
+		dataType: "json",
+		data: {
+			i: spotlightID
+		}
+	}).then(function(response) {
+		console.log(response);
+	});
+};
